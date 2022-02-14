f6337393a8ab3a79907501a9cfe8d65fa22abd7e
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-02-13 18:57:14 -0700

    Primer commit

---
+++ app/src/main/java/acosta/fernando/mifacebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package acosta.fernando.mifacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantalla_2)
+    }
+}
